e0194ce4145e51d9fe11e11ab7cb9996c4a767a6
Simon Mulser <simonmulser@gmail.com>
2017-03-30 10:45:31 +0200

    run pycoin in docker

---
+++ code/src/main.py
@@ -0,0 +1,15 @@
+from bitcoin import network
+
+def on_inv_received(connection, inv):
+    for item in inv.hashes:
+        print(connection.host, item[0], item[1].encode("hex"))
+
+def start():
+    client = network.GeventNetworkClient()
+    client.register_handler('inv', on_inv_received)
+    network.ClientBehavior(client)
+    client.connect(('seed.bitcoinstats.com', 8333))
+    client.run_forever()
+    
+if __name__=="__main__":
+    start()
