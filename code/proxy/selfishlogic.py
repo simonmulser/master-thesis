da5cc7e83073f7a42c8bb41d2b71979a655cbeca
Simon Mulser <simonmulser@gmail.com>
2017-04-22 11:24:52 +0200

    handle orphan blocks

--- code/proxy/selfishlogic.py
+++ code/proxy/selfishlogic.py
@@ -40,34 +40,59 @@ class SelfishLogic:
 
         if received_block.GetHash() in self.known_block_hashes:
             return
+        else:
+            self.known_block_hashes.append(received_block.GetHash())
 
-        parent_block = None
+        prevBlock = None
         for tip in self.tips:
-            if tip.block_hash == received_block.hashPrevBlock:
-                parent_block = tip
+            if tip.hash == received_block.hashPrevBlock:
+                prevBlock = tip
                 break
-        if parent_block is None:
+        if prevBlock is None:
             for block in self.blocks:
-                if block.block_hash == received_block.hashPrevBlock:
-                    parent_block = block
+                if block.hash == received_block.hashPrevBlock:
+                    prevBlock = block
                     break
 
-        block = Block(received_block.GetHash(), parent_block)
+        block = Block(received_block.GetHash(), received_block.hashPrevBlock)
         self.blocks.append(block)
 
-        if parent_block is None:
+        if prevBlock is None:
             self.orphan_blocks.append(block)
         else:
-            if parent_block in self.tips:
-                self.tips.remove(parent_block)
+            self.insert_block(prevBlock, block)
+
+            inserted = True
+            while inserted:
+
+                inserted_orphan_blocks = []
+                for orphan_block in self.orphan_blocks:
+                    if block.hash == orphan_block.hashPrevBlock:
+                        self.insert_block(block, orphan_block)
+                        inserted_orphan_blocks.append(orphan_block)
+                        block = orphan_block
+
+                if len(inserted_orphan_blocks) == 0:
+                    inserted = False
+
+                for inserted_orphan_block in inserted_orphan_blocks:
+                    if inserted_orphan_block in self.orphan_blocks:
+                        self.orphan_blocks.remove(inserted_orphan_block)
+
+    def insert_block(self, prevBlock, block):
+            if prevBlock in self.tips:
+                self.tips.remove(prevBlock)
             self.tips.append(block)
-            parent_block.child_blocks.append(block)
+            prevBlock.child_blocks.append(block)
+            block.height = prevBlock.height + 1
+            block.prevBlock = prevBlock
 
 
 class Block:
 
-    def __init__(self, block_hash, parent):
+    def __init__(self, hash, hashPrevBlock):
         self.child_blocks = []
-        self.block_hash = block_hash
-        self.parent = parent
-        self.height = parent.height + 1 if parent is not None else 0
+        self.hash = hash
+        self.hashPrevBlock = hashPrevBlock
+        self.prevBlock = None
+        self.height = 0
--- code/proxy/tests/test_selfishproxy.py
+++ code/proxy/tests/test_selfishproxy.py
@@ -87,7 +87,6 @@ class SelfishProxyTest(unittest.TestCase):
         logic.process_block(connection, msg)
         logic.process_block(connection, msg)
 
-        self.assertTrue(block.GetHash() in logic.blocks)
         self.assertEqual(len(logic.blocks), 2)
 
     @mock.patch('bitcoinnetwork.network.GeventConnection', autospec=True)
@@ -119,18 +118,55 @@ class SelfishProxyTest(unittest.TestCase):
     @mock.patch('bitcoinnetwork.network.GeventConnection', autospec=True)
     def test_process_fork(self, connection):
         logic = SelfishLogic()
-        first_block = core.CBlock(hashPrevBlock=genesis_hash())
+        first_block = core.CBlock(hashPrevBlock=genesis_hash(), nNonce=1)
         msg = messages.msg_block
         msg.block = first_block
         logic.process_block(connection, msg)
 
-        second_block = core.CBlock(hashPrevBlock=genesis_hash())
+        second_block = core.CBlock(hashPrevBlock=genesis_hash(), nNonce=2)
         msg.block = second_block
         logic.process_block(connection, msg)
 
         self.assertEqual(len(logic.tips), 2)
         self.assertEqual(logic.tips[0].height, 1)
         self.assertEqual(logic.tips[1].height, 1)
+
+    @mock.patch('bitcoinnetwork.network.GeventConnection', autospec=True)
+    def test_process_orphan_blocks(self, connection):
+        logic = SelfishLogic()
+        first_block = core.CBlock(hashPrevBlock=genesis_hash())
+        second_block = core.CBlock(hashPrevBlock=first_block.GetHash())
+        msg = messages.msg_block
+        msg.block = second_block
+        logic.process_block(connection, msg)
+
+        msg.block = first_block
+        logic.process_block(connection, msg)
+
+        self.assertEqual(len(logic.tips), 1)
+        self.assertEqual(logic.tips[0].height, 2)
+        self.assertEqual(logic.tips[0].prevBlock.height, 1)
+
+    @mock.patch('bitcoinnetwork.network.GeventConnection', autospec=True)
+    def test_process_two_orphan_blocks(self, connection):
+        logic = SelfishLogic()
+        first_block = core.CBlock(hashPrevBlock=genesis_hash())
+        second_block = core.CBlock(hashPrevBlock=first_block.GetHash())
+        third_block = core.CBlock(hashPrevBlock=second_block.GetHash())
+
+        msg = messages.msg_block
+        msg.block = third_block
+        logic.process_block(connection, msg)
+
+        msg.block = second_block
+        logic.process_block(connection, msg)
+
+        msg.block = first_block
+        logic.process_block(connection, msg)
+
+        self.assertEqual(len(logic.tips), 1)
+        self.assertEqual(logic.tips[0].height, 3)
+        self.assertEqual(logic.tips[0].prevBlock.height, 2)
 
 
 def get_type_key(msg_type):
