b4e25795d4387672f9c43e487c7c2f1978458744
Simon Mulser <simonmulser@gmail.com>
2017-05-03 16:34:52 +0200

    implement strategy with code

---
+++ code/selfishproxy/strategy/code.py
@@ -0,0 +1,72 @@
+import logging
+from strategy import ForkState
+from strategy import Action
+from strategy import ActionException
+from strategy import BlockOrigin
+
+
+class Strategy:
+    def __init__(self, lead_stubborn=False, equal_fork_stubborn=False, trail_stubborn=0):
+        self.lead_stubborn = lead_stubborn
+        self.equal_fork_stubborn = equal_fork_stubborn
+        self.trail_stubborn = trail_stubborn * -1
+        self.active = False
+
+    def find_action(self, length_private, length_public, last_block_origin):
+        if last_block_origin is BlockOrigin.public:
+            length_public -= 1
+            self.active = False
+        else:
+            length_private -= 1
+
+        private_lead = length_private - length_public
+
+        if private_lead == 0:
+            if last_block_origin is BlockOrigin.public:
+                return Action.adopt
+            else:
+                if self.active:
+                    if self.equal_fork_stubborn:
+                        return Action.wait
+                    else:
+                        return Action.override
+                elif length_private > 0:
+                    return Action.override
+                else:
+                    return Action.wait
+
+        elif private_lead > 0:
+            if private_lead == 1:
+                if last_block_origin is BlockOrigin.public:
+                    self.active = True
+                    return Action.match
+                else:
+                    return Action.wait
+
+            elif private_lead == 2:
+                if last_block_origin is BlockOrigin.public:
+                    if self.lead_stubborn:
+                        self.active = True
+                        return Action.match
+                    else:
+                        return Action.override
+                else:
+                    return Action.wait
+
+            else:
+                if last_block_origin is BlockOrigin.public and self.lead_stubborn:
+                    self.active = True
+                    return Action.match
+                return Action.wait
+
+        else:
+            if last_block_origin is BlockOrigin.private:
+                if private_lead < self.trail_stubborn:
+                    raise ActionException("private_lead={} should not be behind trail_stubborn={}"
+                                          .format(private_lead, self.trail_stubborn))
+                return Action.wait
+            else:
+                if private_lead <= self.trail_stubborn:
+                    return Action.adopt
+                else:
+                    return Action.wait
