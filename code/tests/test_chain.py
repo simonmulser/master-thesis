2f2e4a4cf6b5fe90f13307b50d3f730dda7f41c1
Simon Mulser <simonmulser@gmail.com>
2017-05-18 14:57:57 +0200

    collect test data in class

--- code/tests/test_chain.py
+++ code/tests/test_chain.py
@@ -1,4 +1,4 @@
-import unittest
+import test_abstractchain
 from chain import Chain
 from chain import Block
 from chainutil import Fork
@@ -10,82 +10,22 @@ from strategy import BlockOrigin
 from strategy import ActionException
 
 
-class ChainTest(unittest.TestCase):
+class ChainTest(test_abstractchain.AbstractChainTest):
 
     def __init__(self, *args, **kwargs):
         super(ChainTest, self).__init__(*args, **kwargs)
-
         self.executor = None
         self.strategy = None
         self.chain = None
-        self.first_block_chain_a = None
-        self.second_block_chain_a = None
-        self.third_a_block_chain_a = None
-        self.third_b_block_chain_a = None
-        self.fourth_block_chain_a = None
-        self.first_block_chain_b = None
-        self.second_block_chain_b = None
-        self.third_a_block_chain_b = None
-        self.third_b_block_chain_b = None
-        self.fourth_block_chain_b = None
 
     def setUp(self):
+        super(ChainTest, self).setUp()
         self.executor = MagicMock()
         self.strategy = MagicMock()
         self.chain = Chain(self.executor, self.strategy)
         self.chain.strategy = MagicMock()
 
-        self.first_block_chain_a = Block(None, BlockOrigin.private)
-        self.first_block_chain_a.height = 1
-        self.first_block_chain_a.prevBlock = chain.genesis_block
-        self.first_block_chain_a.cached_hash = '1a'
-
-        self.second_block_chain_a = Block(None, BlockOrigin.private)
-        self.second_block_chain_a.height = 2
-        self.second_block_chain_a.prevBlock = self.first_block_chain_a
-        self.second_block_chain_a.cached_hash = '2a'
-
-        self.third_a_block_chain_a = Block(None, BlockOrigin.private)
-        self.third_a_block_chain_a.height = 3
-        self.third_a_block_chain_a.prevBlock = self.second_block_chain_a
-        self.third_a_block_chain_a.cached_hash = '3a_1'
-
-        self.third_b_block_chain_a = Block(None, BlockOrigin.private)
-        self.third_b_block_chain_a.height = 3
-        self.third_b_block_chain_a.prevBlock = self.second_block_chain_a
-        self.third_b_block_chain_a.cached_hash = '3a_2'
-
-        self.fourth_block_chain_a = Block(None, BlockOrigin.private)
-        self.fourth_block_chain_a.height = 4
-        self.fourth_block_chain_a.prevBlock = self.third_a_block_chain_a
-        self.fourth_block_chain_a.cached_hash = '4a'
-
-        self.first_block_chain_b = Block(None, BlockOrigin.public)
-        self.first_block_chain_b.height = 1
-        self.first_block_chain_b.prevBlock = chain.genesis_block
-        self.first_block_chain_b.cached_hash = '1b'
-
-        self.second_block_chain_b = Block(None, BlockOrigin.public)
-        self.second_block_chain_b.height = 2
-        self.second_block_chain_b.prevBlock = self.first_block_chain_b
-        self.second_block_chain_b.cached_hash = '2b'
-
-        self.third_a_block_chain_b = Block(None, BlockOrigin.public)
-        self.third_a_block_chain_b.height = 3
-        self.third_a_block_chain_b.prevBlock = self.second_block_chain_b
-        self.third_a_block_chain_b.cached_hash = '3b_1'
-
-        self.third_b_block_chain_b = Block(None, BlockOrigin.public)
-        self.third_b_block_chain_b.height = 3
-        self.third_b_block_chain_b.prevBlock = self.second_block_chain_b
-        self.third_b_block_chain_b.cached_hash = '3b_2'
-
-        self.fourth_block_chain_b = Block(None, BlockOrigin.public)
-        self.fourth_block_chain_b.height = 4
-        self.fourth_block_chain_b.prevBlock = self.third_a_block_chain_b
-        self.fourth_block_chain_b.cached_hash = '4b'
-
-    def test_try_to_insert_block_without_prevHash(self):
+    def test_try_to_insert_block_without_prev_hash(self):
         block = core.CBlock()
         self.chain.try_to_insert_block(block, BlockOrigin.public)
 
--- code/tests/test_chainutil.py
+++ code/tests/test_chainutil.py
@@ -1,86 +1,13 @@
-import unittest
-from chain import Block
-from chain import Chain
+import test_abstractchain
 import chain
 import chainutil
-from mock import MagicMock
 from strategy import BlockOrigin
 
 
-class ChainUtilTest(unittest.TestCase):
+class ChainUtilTest(test_abstractchain.AbstractChainTest):
 
     def __init__(self, *args, **kwargs):
         super(ChainUtilTest, self).__init__(*args, **kwargs)
-
-        self.executor = None
-        self.strategy = None
-        self.chain = None
-        self.first_block_chain_a = None
-        self.second_block_chain_a = None
-        self.third_a_block_chain_a = None
-        self.third_b_block_chain_a = None
-        self.fourth_block_chain_a = None
-        self.first_block_chain_b = None
-        self.second_block_chain_b = None
-        self.third_a_block_chain_b = None
-        self.third_b_block_chain_b = None
-        self.fourth_block_chain_b = None
-
-    def setUp(self):
-        self.executor = MagicMock()
-        self.strategy = MagicMock()
-        self.chain = Chain(self.executor, self.strategy)
-        self.chain.strategy = MagicMock()
-
-        self.first_block_chain_a = Block(None, BlockOrigin.private)
-        self.first_block_chain_a.height = 1
-        self.first_block_chain_a.prevBlock = chain.genesis_block
-        self.first_block_chain_a.cached_hash = '1a'
-
-        self.second_block_chain_a = Block(None, BlockOrigin.private)
-        self.second_block_chain_a.height = 2
-        self.second_block_chain_a.prevBlock = self.first_block_chain_a
-        self.second_block_chain_a.cached_hash = '2a'
-
-        self.third_a_block_chain_a = Block(None, BlockOrigin.private)
-        self.third_a_block_chain_a.height = 3
-        self.third_a_block_chain_a.prevBlock = self.second_block_chain_a
-        self.third_a_block_chain_a.cached_hash = '3a_1'
-
-        self.third_b_block_chain_a = Block(None, BlockOrigin.private)
-        self.third_b_block_chain_a.height = 3
-        self.third_b_block_chain_a.prevBlock = self.second_block_chain_a
-        self.third_b_block_chain_a.cached_hash = '3a_2'
-
-        self.fourth_block_chain_a = Block(None, BlockOrigin.private)
-        self.fourth_block_chain_a.height = 4
-        self.fourth_block_chain_a.prevBlock = self.third_a_block_chain_a
-        self.fourth_block_chain_a.cached_hash = '4a'
-
-        self.first_block_chain_b = Block(None, BlockOrigin.public)
-        self.first_block_chain_b.height = 1
-        self.first_block_chain_b.prevBlock = chain.genesis_block
-        self.first_block_chain_b.cached_hash = '1b'
-
-        self.second_block_chain_b = Block(None, BlockOrigin.public)
-        self.second_block_chain_b.height = 2
-        self.second_block_chain_b.prevBlock = self.first_block_chain_b
-        self.second_block_chain_b.cached_hash = '2b'
-
-        self.third_a_block_chain_b = Block(None, BlockOrigin.public)
-        self.third_a_block_chain_b.height = 3
-        self.third_a_block_chain_b.prevBlock = self.second_block_chain_b
-        self.third_a_block_chain_b.cached_hash = '3b_1'
-
-        self.third_b_block_chain_b = Block(None, BlockOrigin.public)
-        self.third_b_block_chain_b.height = 3
-        self.third_b_block_chain_b.prevBlock = self.second_block_chain_b
-        self.third_b_block_chain_b.cached_hash = '3b_2'
-
-        self.fourth_block_chain_b = Block(None, BlockOrigin.public)
-        self.fourth_block_chain_b.height = 4
-        self.fourth_block_chain_b.prevBlock = self.third_a_block_chain_b
-        self.fourth_block_chain_b.cached_hash = '4b'
 
     def test_get_private_public_fork_no_private_tip(self):
         fork = chainutil.get_private_public_fork([self.second_block_chain_b])
